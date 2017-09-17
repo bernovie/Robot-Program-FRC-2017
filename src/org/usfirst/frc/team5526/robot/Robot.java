3c6ea8e86b4f810feb9f97dbf44741a7553609fc
bernovie <bearnyoviedo@gmail.com>
2017-09-17 17:30:55 -0500

    Added comments in Robot.java

--- src/org/usfirst/frc/team5526/robot/Robot.java
+++ src/org/usfirst/frc/team5526/robot/Robot.java
@@ -21,21 +21,16 @@ import edu.wpi.first.wpilibj.Victor;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-/**
- * This is a demo program showing the use of the RobotDrive class, specifically
- * it contains the code necessary to operate a robot with tank drive.
- *
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the SampleRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the manifest file in the resource
- * directory.
- *
- * WARNING: While it may look like a good choice to use for your code if you're
- * inexperienced, don't. Unless you know what you are doing, complex code will
- * be much more difficult under this system. Use IterativeRobot or Command-Based
- * instead if you're new.
- */
+/*
+By: Bernardo Oviedo
+
+Code for the 2017 First Robotics Competition.
+The program is open source and free to use.
+
+NOTE: it is recommended not to directly copy the code but rather take the core concepts
+from different parts of the code that can later be implemented in your own robot. 
+*/
+
 public class Robot extends SampleRobot {
 	
 	//----------------------------------OBJECTS---------------------
